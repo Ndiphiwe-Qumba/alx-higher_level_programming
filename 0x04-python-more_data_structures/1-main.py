cf89132eb42b96d190884367481bd757953060da
Ndiphiwe-Qumba <NdiphiweQumba.nq@gmail.com>
2022-12-09 05:08:33 -0800

    Search

---
+++ 0x04-python-more_data_structures/1-main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+search_replace = __import__('1-search_replace').search_replace
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"1-main.py" [New File]                                        0,0-1         All
+
